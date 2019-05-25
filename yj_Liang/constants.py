27c46980e0122765e4062eddf0580b52eaac9c71
luciaicull <lj2141301@gmail.com>
2019-05-25 16:35:45 +0000

    add 2 methods for extracting excerpt information

--- yj_Liang/constants.py
+++ yj_Liang/constants.py
@@ -5,6 +5,11 @@ WIN_LENGTH = 1024
 HOP_LENGTH = 512 # determines number of frames
 NUM_MELS = 128
 
+TRIM_SECOND_BEFORE = 0.2
+TRIM_SECOND_AFTER = 0.3
+ONSET_INPUT_SHAPE = (1, int(SAMPLING_RATE * (TRIM_SECOND_BEFORE + TRIM_SECOND_AFTER)))
+MIN_SRC = 0.3
+MAX_SRC = 2.3
 
 # training parameters
 
@@ -17,10 +22,7 @@ TRAIN_YEARS = ['2004', '2006', '2008', '2009', '2013', '2014', '2015', '2017']
 TEST_YEARS = ['2011']
 
 DATA_PATH = 'yj_dataset/'
+CONVERTED_WAVE_PATH = 'converted_wavefile/'  # to rebuild Beici's paper
 NON_Pedal_DATA_PATH = DATA_PATH + 'non_pedal/'
-
-# to rebuild Beici's paper
-PEDAL_MIDI_PATH = ORIGINAL_DATA_PATH
-CONVERTED_WAVE_PATH = ORIGINAL_DATA_PATH + DATA_PATH + 'converted_wavefile/'
 
 SETS = ['train', 'test']
--- yj_Liang/data_preparation.py
+++ yj_Liang/data_preparation.py
@@ -2,6 +2,7 @@ import os
 import librosa
 import numpy as np
 import pretty_midi
+import csv
 from constants import *
 
 def get_non_pedal_midi():
@@ -64,6 +65,148 @@ def train_test_split():
     organize_file_names(TEST_YEARS)
 
 
+def pedal_onset_preparation():
+    print("Start creating pedal-onset excerpts")
+
+
+    for SET in SETS:
+        f = open(ORIGINAL_DATA_PATH + DATA_PATH + SET + '.txt', 'r')
+        file_names = f.readlines()
+        f_pedal = open(ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + SET + '_onset_p.csv', 'w')
+        f_nonpedal = open(ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + SET + '_onset_np.csv', 'w')
+        p_writer = csv.writer(f_pedal)
+        np_writer = csv.writer(f_nonpedal)
+        for file_name in file_names:
+            file_name = file_name.split('\n')[0]
+            print('      ' + file_name)
+            midi_path = ORIGINAL_DATA_PATH + file_name + '.midi'
+            pedal_path = ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + 'pedal/' + file_name + '.wav'
+            non_pedal_path = ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + 'non-pedal/' + file_name + '.wav'
+
+            pedal_audio, sr = librosa.load(pedal_path, sr=SAMPLING_RATE)
+            non_pedal_audio, sr = librosa.load(non_pedal_path, sr=SAMPLING_RATE)
+
+            midi = pretty_midi.PrettyMIDI(midi_path)
+            midi_inst = midi.instruments[0]
+            pedal_value = []
+            pedal_time = []
+            # detect sustain pedal(control change number 64)
+            for control_change in midi_inst.control_changes:
+                if control_change.number == 64:
+                    pedal_value.append(control_change.value)
+                    pedal_time.append(control_change.time)
+
+            pedal_onset_time = []
+            # detect sustain pedal onset(control change value above 64)
+            for idx, value in enumerate(pedal_value):
+                if idx > 0 and value >= 64 and pedal_value[idx-1] < 64:
+                    pedal_onset_time.append(pedal_time[idx])
+
+
+            pedal_onset_sample = librosa.time_to_samples(pedal_onset_time, sr=SAMPLING_RATE)
+            for seg_idx, sample in enumerate(pedal_onset_sample):
+                start = int(sample - TRIM_SECOND_BEFORE * SAMPLING_RATE)
+                end = int(sample + TRIM_SECOND_AFTER * SAMPLING_RATE)
+                #new_file_name = file_name.replace('/', '-')
+
+                if start > 0 and end < len(non_pedal_audio):
+                    # cut pedal excerpt and store its information in another file
+                    # cannot store all excerpt audios itself because of lack of storage
+                    '''
+                    pedal_excerpt_name = '{}-p_{}.wav'.format(new_file_name, seg_idx)
+                    pedal_excerpt_path = ORIGINAL_DATA_PATH + DATA_PATH + SET + '/pedal_onset/' + pedal_excerpt_name
+                    librosa.output.write_wav(pedal_excerpt_path, pedal_audio[start:end], SAMPLING_RATE)
+                    '''
+                    p_writer.writerow([file_name + '.wav', str(start), str(end)])
+
+                    # cut non_pedal excerpt and store its information in another file
+                    # cannot store all excerpt audios itself because of lack of storage
+                    '''
+                    non_pedal_excerpt_name = '{}-np_{}.wav'.format(new_file_name, seg_idx)
+                    non_pedal_excerpt_path = ORIGINAL_DATA_PATH + DATA_PATH + SET + '/nonpedal_onset/' + non_pedal_excerpt_name
+                    librosa.output.write_wav(non_pedal_excerpt_path, non_pedal_audio[start:end], SAMPLING_RATE)
+                    '''
+                    np_writer.writerow([file_name + '.wav', str(start), str(end)])
+
+    print("End creating pedal-onset excerpts")
+
+
+
+def pedal_segment_preparation():
+    print("Start creating pedal-segment excerpts")
+
+    min_sp = int(MIN_SRC * SAMPLING_RATE)
+    max_sp = int(MAX_SRC * SAMPLING_RATE)
+
+    for SET in SETS:
+        f = open(ORIGINAL_DATA_PATH + DATA_PATH + SET + '.txt', 'r')
+        file_names = f.readlines()
+
+        f_pedal = open(ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + SET + '_segment_p.csv', 'w')
+        f_nonpedal = open(ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + SET + '_segment_np.csv', 'w')
+        p_writer = csv.writer(f_pedal)
+        np_writer = csv.writer(f_nonpedal)
+
+        for file_name in file_names:
+            file_name = file_name.split('\n')[0]
+            print('      ' + file_name)
+            midi_path = ORIGINAL_DATA_PATH + file_name + '.midi'
+
+            midi = pretty_midi.PrettyMIDI(midi_path)
+            midi_inst = midi.instruments[0]
+            pedal_value = []
+            pedal_time = []
+            # detect sustain pedal(control change number 64)
+            for control_change in midi_inst.control_changes:
+                if control_change.number == 64:
+                    pedal_value.append(control_change.value)
+                    pedal_time.append(control_change.time)
+
+            pedal_onset_time = []
+            pedal_offset_time = []
+            # detect sustain pedal onset(control change value above 64) and offset(control change value under 64)
+            for idx, value in enumerate(pedal_value):
+                if idx > 0 and value >= 64 and pedal_value[idx-1] < 64:
+                    pedal_onset_time.append(pedal_time[idx])
+                elif idx > 0 and value < 64 and pedal_value[idx-1] >= 64:
+                    pedal_offset_time.append(pedal_time[idx])
+
+            pedal_offset_time = [t for t in pedal_offset_time if t > pedal_onset_time[0]]
+            segment_idxs = np.min([len(pedal_onset_time), len(pedal_offset_time)])
+            pedal_onset_time = pedal_onset_time[:segment_idxs]
+            pedal_offset_time = pedal_offset_time[:segment_idxs]
+
+            correct_pedal_data = False
+            for seg_idx, offset_time in enumerate(pedal_offset_time):
+                if offset_time != pedal_offset_time[-1] and offset_time > pedal_onset_time[seg_idx] and offset_time < pedal_onset_time[seg_idx+1]:
+                    correct_pedal_data = True
+                elif offset_time == pedal_offset_time[-1] and offset_time > pedal_onset_time[seg_idx]:
+                    correct_pedal_data = True
+
+            if correct_pedal_data:
+                pedal_onset_sample = librosa.time_to_samples(pedal_onset_time, sr=SAMPLING_RATE)
+                pedal_offset_sample = librosa.time_to_samples(pedal_offset_time, sr=SAMPLING_RATE)
+
+                pedal_path = ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + 'pedal/' + file_name + '.wav'
+                non_pedal_path = ORIGINAL_DATA_PATH + DATA_PATH + CONVERTED_WAVE_PATH + 'non-pedal/' + file_name + '.wav'
+
+                pedal_audio, sr = librosa.load(pedal_path, sr=SAMPLING_RATE)
+                non_pedal_audio, sr = librosa.load(non_pedal_path, sr=SAMPLING_RATE)
+
+                for seg_idx, start in enumerate(pedal_onset_sample):
+                    end = pedal_offset_sample[seg_idx]
+                    length = end - start
+                    if length > max_sp:
+                        end = start + max_sp
+
+                    if length >= min_sp and end < len(non_pedal_audio):
+                        p_writer.writerow([file_name + '.wav', str(start), str(end)])
+                        np_writer.writerow([file_name + '.wav', str(start), str(end)])
+
+    print("End creating pedal-segment excerpts")
+
+
+
 def melspectrogram(file_name):
     y, sr = librosa.load(ORIGINAL_DATA_PATH + file_name, SAMPLING_RATE)
     S = librosa.stft(y, n_fft=N_FFT, hop_length=HOP_LENGTH, win_length=WIN_LENGTH)
@@ -93,4 +236,5 @@ if __name__ == '__main__':
     #get_file_list_txt()
     #train_test_split()
     #extract_features()
-    get_non_pedal_midi()
+    #get_non_pedal_midi()
+    pedal_onset_preparation()
