a20414223056163dce2e09a0dc3a5ed0f4d19382
luciaicull <lj2141301@gmail.com>
2019-05-30 04:29:30 +0000

    first commit of model implementation

--- yj_Liang/models.py
+++ yj_Liang/models.py
@@ -5,4 +5,54 @@ class OnsetConv(nn.Module):
         super(OnsetConv, self).__init__()
 
         self.multipleConv1 = nn.Conv2d()
+        self.multipleConv2 = nn.Conv2d()
+        self.multipleConv3 = nn.Conv2d()
 
+        self.concat = nn.Sequential(
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+            nn.Dropout(),
+        )
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+            nn.Dropout()
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+            nn.Dropout()
+        )
+
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.ReLU(),
+            nn.MaxPool2d(),
+            nn.Dropout()
+        )
+
+
+    def forward(self, mel):
+        x = mel.view(mel.size(0), 1, mel.size(1), mel.size(2))
+
+
+        x1 = self.multipleConv1(x)
+        x2 = self.multipleConv2(x)
+        x3 = self.multipleConv3(x)
+
+
+
+class SegmentConv(nn.Module):
+    def __init__(self):
+        super(SegmentConv, self).__init__()
+
+    def forward(self, mel):
+        pass
